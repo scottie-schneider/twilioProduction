fd53c3aceb37ce176b99d4f6cbcfb5b793cabb44
Scottie Schneider <scott@laclave.co>
2018-06-13 22:15:56 -0500

    easy

--- index.js
+++ index.js
@@ -13,16 +13,27 @@ app.use(urlencoded({ extended: false }));
 // HTTP POST to /voice in our application
 app.post('/voice', twilio.webhook({validate: false}), function(req, res, next) {
   console.log(req.body);
-  var phoneNumber = '5128176951';
-  var callerId = '5128176776';
-  var twiml = new VoiceResponse();
-
-  var dial = twiml.dial({callerId : callerId});
-  if (phoneNumber != null) {
-    dial.number(phoneNumber);
-  }
 
   res.send(twiml.toString());
+  // start bubble call
+  let token = '98107ac3b7b363d93f1b9e3863b79bee;';
+      axios.get(`https://followupedge.com/version-test/api/1.1/obj/user?constraints=%5B%7B%22key%22%3A%22CampaignPhone%22%2C%22constraint_type%22%3A%22equals%22%2C%22value%22%3A%22${SearchNum}%22%7D%5D`,
+        {
+          headers: {'Authorization': `Bearer ${token}`}
+      })
+      .then((res) => {
+        var phoneNumber = '5128176951';
+        var callerId = '5128176776';
+        var twiml = new VoiceResponse();
+
+        var dial = twiml.dial({callerId : callerId});
+        if (phoneNumber != null) {
+          dial.number(phoneNumber);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      })
 });
 
 // Create an HTTP server and listen for requests on port 3000
