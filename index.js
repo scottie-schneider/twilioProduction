1c46e205c145c5bb8226a4546e72e2f937a5b9b5
Scottie Schneider <scott@laclave.co>
2018-06-13 22:48:42 -0500

    easy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const urlencoded = require('body-parser').urlencoded;
-const twilio = require('twilio');
+const twilio = require('twilio')
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -12,13 +12,15 @@ app.use(urlencoded({ extended: false }));
 // Create a route that will handle Twilio webhook requests, sent as an
 // HTTP POST to /voice in our application
 app.post('/voice', twilio.webhook({validate: false}), function(req, res, next) {
-  var phoneNumber = '5128176951';
-  var callerId = '5128176776';
+  var phoneNumber = '15128176951';
+  var callerId = '15128176776';
   var twiml = new VoiceResponse();
 
   var dial = twiml.dial({callerId : callerId});
   if (phoneNumber != null) {
     dial.number(phoneNumber);
+  } else {
+    dial.client("support_agent");
   }
 
   res.send(twiml.toString());
