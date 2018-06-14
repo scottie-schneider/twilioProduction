7bd43e13d31960d4854345855aa0ac9806aed5ae
Scottie Schneider <scott@laclave.co>
2018-06-13 23:16:16 -0500

    easy

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-const express = require('express');
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
-const urlencoded = require('body-parser').urlencoded;
-const twilio = require('twilio')
-const app = express();
 
-app.set('port', (process.env.PORT || 5000));
 
-// Parse incoming POST params with Express middleware
-app.use(urlencoded({ extended: false }));
+var express = require('express');
+var twilio = require('twilio');
+var VoiceResponse = twilio.twiml.VoiceResponse;
 
-// Create a route that will handle Twilio webhook requests, sent as an
-// HTTP POST to /voice in our application
-app.post('/voice', (req, res) => {
+// POST /calls/connect
+router.post('/connect', twilio.webhook({validate: false}), function(req, res, next) {
   var phoneNumber = '+15128176951';
   var callerId = '+15128176776';
-  const twiml = new VoiceResponse();
-  var dial = twiml.dial({callerId : callerId});
-  dial.number(phoneNumber);
-  res.type('text/xml');
-  res.send(twiml.toString());
-});
+  var twiml = new VoiceResponse();
 
-// Create an HTTP server and listen for requests on port 3000
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  var dial = twiml.dial({callerId : callerId});
+  if (phoneNumber != null) {
+    dial.number(phoneNumber);
+  }
+
+  res.send(twiml.toString());
 });
