750dd3485784c28130fedc7d01977fb6ede9295a
Scottie Schneider <scott@laclave.co>
2018-06-13 22:09:47 -0500

    easy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const urlencoded = require('body-parser').urlencoded;
-
+const twilio = require('twilio');
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -13,14 +13,12 @@ app.use(urlencoded({ extended: false }));
 // HTTP POST to /voice in our application
 app.post('/voice', twilio.webhook({validate: false}), function(req, res, next) {
   var phoneNumber = req.body.phoneNumber;
-  var callerId = process.env.TWILIO_PHONE_NUMBER;
+  var callerId = '5128176776';
   var twiml = new VoiceResponse();
 
   var dial = twiml.dial({callerId : callerId});
   if (phoneNumber != null) {
     dial.number(phoneNumber);
-  } else {
-    dial.client("support_agent");
   }
 
   res.send(twiml.toString());
