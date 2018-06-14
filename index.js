5fe0ed91c4e2deb2d31eb7c63b95f96fc1b4948a
Scottie Schneider <scott@laclave.co>
2018-06-13 23:30:30 -0500

    starting again

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(urlencoded({ extended: false }));
 // Create a route that will handle Twilio webhook requests, sent as an
 // HTTP POST to /voice in our application
 app.post('/', (request, response) => {
-  var phoneNumber = '+15125984144';
-  var callerId = '+15128176951';
+  var phoneNumber = '+15128176951';
+  var callerId = '+15125984144';
   var twiml = new VoiceResponse();
 
   var dial = twiml.dial({callerId : callerId});
