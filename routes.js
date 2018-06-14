822ad527f0deb36858925255d296bcf0164a6db4
Scottie Schneider <scott@laclave.co>
2018-06-13 23:27:31 -0500

    starting again

--- routes.js
+++ routes.js
@@ -3,25 +3,28 @@ const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const urlencoded = require('body-parser').urlencoded;
 
 const app = express();
-
+app.set('port', (process.env.PORT || 5000));
 // Parse incoming POST params with Express middleware
 app.use(urlencoded({ extended: false }));
 
 // Create a route that will handle Twilio webhook requests, sent as an
 // HTTP POST to /voice in our application
 app.post('/', (request, response) => {
-  // Get information about the incoming call, like the city associated
-  // with the phone number (if Twilio can discover it)
+  var phoneNumber = '+15125984144';
+  var callerId = '+15128176776';
+  var twiml = new VoiceResponse();
 
-  // Use the Twilio Node.js SDK to build an XML response
-  const twiml = new VoiceResponse();
-  twiml.say({ voice: 'alice' }, `Never gonna give you up`);
-  twiml.play({}, 'https://demo.twilio.com/docs/classic.mp3');
+  var dial = twiml.dial({callerId : callerId});
+  if (phoneNumber != null) {
+    dial.number(phoneNumber);
+  } else {
+    dial.client("support_agent");
+  }
 
-  // Render the response as XML in reply to the webhook request
-  response.type('text/xml');
-  response.send(twiml.toString());
+res.send(twiml.toString());
 });
 
 // Create an HTTP server and listen for requests on port 3000
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
