7552ac1103bbfd1c451ab742ee498959a48f3d3b
Scottie Schneider <scott@laclave.co>
2018-06-13 22:40:58 -0500

    easy

--- index.js
+++ index.js
@@ -16,28 +16,29 @@ app.use(urlencoded({ extended: false }));
 app.post('/voice', twilio.webhook({validate: false}), function(req, res, next) {
   console.log(req);
 
-  // start bubble call
-  let token = '98107ac3b7b363d93f1b9e3863b79bee';
-  let searchNum = '15125984144';
-    axios.get(`https://followupedge.com/version-test/api/1.1/obj/user?constraints=%5B%7B%22key%22%3A%22CampaignPhone%22%2C%22constraint_type%22%3A%22equals%22%2C%22value%22%3A%22${searchNum}%22%7D%5D`,
-      {
-        headers: {'Authorization': `Bearer ${token}`}
-    })
-    .then((res) => {
-      var phoneNumber = '5128176951';
-      var callerId = '5128176776';
-      var twiml = new VoiceResponse();
+  var phoneNumber = '5128176951';
+  var callerId = '5128176776';
+  var twiml = new VoiceResponse();
 
-      var dial = twiml.dial({callerId : callerId});
-      if (phoneNumber != null) {
-        dial.number(phoneNumber);
-      }
-      //res.send(twiml.toString());
-    })
-    .catch((e) => {
-      console.log(e.message);
-    })
-});
+  var dial = twiml.dial({callerId : callerId});
+  if (phoneNumber != null) {
+    dial.number(phoneNumber);
+  }
+  //res.send(twiml.toString());
+  // start bubble call
+//   let token = '98107ac3b7b363d93f1b9e3863b79bee';
+//   let searchNum = '15125984144';
+//     axios.get(`https://followupedge.com/version-test/api/1.1/obj/user?constraints=%5B%7B%22key%22%3A%22CampaignPhone%22%2C%22constraint_type%22%3A%22equals%22%2C%22value%22%3A%22${searchNum}%22%7D%5D`,
+//       {
+//         headers: {'Authorization': `Bearer ${token}`}
+//     })
+//     .then((res) => {
+//
+//     })
+//     .catch((e) => {
+//       console.log(e.message);
+//     })
+// });
 
 // Create an HTTP server and listen for requests on port 3000
 app.listen(app.get('port'), function() {
