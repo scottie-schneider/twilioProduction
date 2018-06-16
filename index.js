9ea292ae5a18a25a1fd6079731c0c9a9398155a0
Scottie Schneider <scott@laclave.co>
2018-06-16 15:46:27 -0500

    added support for Aussie number call throughs

--- index.js
+++ index.js
@@ -8,34 +8,54 @@ app.set('port', (process.env.PORT || 5000));
 // Parse incoming POST params with Express middleware
 app.use(urlencoded({ extended: false }));
 
-
+app.get('/', (request, response) => {
+  response.send('hello world');
+})
 // Create a route that will handle Twilio webhook requests, sent as an
 // HTTP POST to /voice in our application
 app.post('/voice', (request, response) => {
 async function go(){
   try{
-    const wes = await axios(`https://followupedge.com/api/1.1/obj/user?api_token=98107ac3b7b363d93f1b9e3863b79bee&constraints=%5B%7B%22key%22%3A%22CampaignPhone%22%2C%22constraint_type%22%3A%22equals%22%2C%22value%22%3A%22${request.body.To.substring(1)}%22%7D%5D`);
-    console.log(wes.data.response.results[0].personalPhone);
-    console.log(request.body.From);
-    var phoneNumber = wes.data.response.results[0].personalPhone;
-    var callerId = request.body.From;
-    var twiml = new VoiceResponse();
+    let campaignPhone = request.body.To;
+    //if the first 3 characters are +61, make it an aussie number
+    console.log('campaign phone below');
+    console.log(campaignPhone)
 
-    var dial = twiml.dial({callerId : callerId});
-    if (phoneNumber != null) {
-      dial.number(phoneNumber);
-    } else {
-      dial.client("support_agent");
+    if(campaignPhone.slice(0,3) == "+61"){
+      campaignPhone = request.body.To.substring(3);
+      console.log(`aussie number ${campaignPhone}`)
     }
-    response.send(twiml.toString());
-    // TODO: hit bubble endpoint
-    axios.post('https://followupedge.com/api/1.1/wf/gotacall', {
-    user: wes.data.response.results[0]._id,
-    prospectPhone: request.body.From
-  })
-  .catch((e) => {
-    console.log(e.message);
-  })
+    // if the first 2 characters are +1, then it's a us/canada number and proceed as normal
+    if(campaignPhone.slice(0,2) == "+1"){
+      campaignPhone = request.body.To.substring(1);
+      console.log(`US number, ${campaignPhone}`)
+    }
+    const wes = await axios(`https://followupedge.com/api/1.1/obj/user?api_token=98107ac3b7b363d93f1b9e3863b79bee&constraints=%5B%7B%22key%22%3A%22CampaignPhone%22%2C%22constraint_type%22%3A%22equals%22%2C%22value%22%3A%22${campaignPhone}%22%7D%5D`);
+    if(wes.data.response.results.length == 0){
+      console.log('not found');
+      response.send('not found')
+    }else {
+      console.log(wes.data.response.results);
+      console.log(wes.data.response.results[0].personalPhone);
+      console.log(request.body.From);
+      var phoneNumber = wes.data.response.results[0].personalPhone;
+      var callerId = request.body.From;
+      var twiml = new VoiceResponse();
+
+      var dial = twiml.dial({callerId : callerId});
+      dial.number(phoneNumber);
+
+      response.send(twiml.toString());
+      // TODO: hit bubble endpoint
+      axios.post('https://followupedge.com/api/1.1/wf/gotacall', {
+        user: wes.data.response.results[0]._id,
+        prospectPhone: request.body.From
+      })
+      .catch((e) => {
+        console.log('you have an error')
+        console.log(e.message);
+      })
+    }
   }catch (e){
     console.log(e);
   }
--- test.js
+++ test.js
@@ -1,65 +1,49 @@
-/**
- *  Call Forward Template
- *
- *  This Function will forward a call to another phone number. If the call isn't answered or the line is busy,
- *  the call is optionally forwarded to a specified URL. You can optionally restrict which calling phones
- *  will be forwarded.
- */
+const express = require('express');
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const urlencoded = require('body-parser').urlencoded;
+const axios = require('axios'); // promised based requests - like fetch()
 
-exports.handler = function(context, event, callback) {
-  // async call to bubble to get user
+const app = express();
+app.set('port', (process.env.PORT || 5000));
+// Parse incoming POST params with Express middleware
+app.use(urlencoded({ extended: false }));
 
-  let token = '98107ac3b7b363d93f1b9e3863b79bee;'
-  axios.get(`https://followupedge.com/version-test/api/1.1/obj/user?constraints=%5B%7B%22key%22%3A%22CampaignPhone%22%2C%22constraint_type%22%3A%22equals%22%2C%22value%22%3A%22${campaignNum}%22%7D%5D`,
-  {
-  },
-    {
-      headers: {'Authorization': `Bearer ${token}`}
+
+// Create a route that will handle Twilio webhook requests, sent as an
+// HTTP POST to /voice in our application
+app.post('/voice', (request, response) => {
+async function go(){
+  try{
+    const wes = await axios(`https://followupedge.com/api/1.1/obj/user?api_token=98107ac3b7b363d93f1b9e3863b79bee&constraints=%5B%7B%22key%22%3A%22CampaignPhone%22%2C%22constraint_type%22%3A%22equals%22%2C%22value%22%3A%22${request.body.To.substring(1)}%22%7D%5D`);
+    console.log(wes.data.response.results[0].personalPhone);
+    console.log(request.body.From);
+    var phoneNumber = wes.data.response.results[0].personalPhone;
+    var callerId = request.body.From;
+    var twiml = new VoiceResponse();
+
+    var dial = twiml.dial({callerId : callerId});
+    if (phoneNumber != null) {
+      dial.number(phoneNumber);
+    } else {
+      dial.client("support_agent");
+    }
+    response.send(twiml.toString());
+    // TODO: hit bubble endpoint
+    axios.post('https://followupedge.com/api/1.1/wf/gotacall', {
+    user: wes.data.response.results[0]._id,
+    prospectPhone: request.body.From
   })
-  .then((res) => {
-    console.log(`success`)
-
-    // REQUIRED - you must set this
-    let phoneNumber = res.data.response.results[0].personalPhone;
-    // OPTIONAL
-    let callerId =  event.CallerId || null;
-    // OPTIONAL
-    let timeout = event.Timeout || null;
-    // OPTIONAL
-    let allowedCallers = event.allowedCallers || [];
-
-    // generate the TwiML to tell Twilio how to forward this call
-    let twiml = new Twilio.twiml.VoiceResponse();
-
-    let allowedThrough = true
-    if (allowedCallers.length > 0) {
-      if (allowedCallers.indexOf(event.From) === -1) {
-        allowedThrough = false;
-      }
-    }
-
-    let dialParams = {};
-    if (callerId) {
-      dialParams.callerId = callerId
-    }
-    if (timeout) {
-      dialParams.timeout = timeout
-    }
-
-    if (allowedThrough) {
-      twiml.dial(dialParams, phoneNumber);
-    }
-    else {
-      twiml.say('Sorry, you are calling from a restricted number. Good bye.');
-    }
-    return twiml;
-})
   .catch((e) => {
-    console.log(e);
-    console.log('fire the second forward')
+    console.log(e.message);
   })
+  }catch (e){
+    console.log(e);
+  }
+}
+  go();
 });
 
-  // return the TwiML
-  callback(null, twiml);
-};
+// Create an HTTP server and listen for requests on port 3000
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
